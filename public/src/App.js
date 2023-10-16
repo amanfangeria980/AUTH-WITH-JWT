7f82ff46107f24830e102cd891c0d11cc2cf08bc
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 07:04:19 +0530

    did routing for all the pages

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Secret from "./pages/Secret";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+    <h1>Home Page</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/register" element={<Register />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/secret" element={<Secret />}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
