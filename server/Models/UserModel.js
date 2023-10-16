c15e8b139f77c0e76eb8649cc7fc93862a227b4b
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 23:16:07 +0530

    password encryption done before saving it to db

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -10,6 +10,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Password is required*"],
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("Users", userSchema);
