0d2be1888308cb58c45fe1f5d59cf78f9c04f643
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 23:28:27 +0530

    toast is working fine

--- public/src/App.js
+++ public/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Secret from "./pages/Secret";
 import Login from "./pages/Login";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
