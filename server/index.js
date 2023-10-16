e79edae3854622770b41e3cfdb2a2e8ca035e7f9
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 06:55:18 +0530

    added gitignore and db working

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    method: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
 app.listen(4000, () => {
-  "Server started on Port 4000";
+  console.log("Server started on Port 4000");
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/jwt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Success!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
