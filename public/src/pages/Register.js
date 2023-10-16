7e167cca7b5342a3db05375ff092128f703bac04
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 23:08:00 +0530

    jwt cookie storing in browser now

--- public/src/pages/Register.js
+++ public/src/pages/Register.js
@@ -11,9 +11,15 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("http://localhost:4000/register", {
-        ...values,
-      });
+      const { data } = await axios.post(
+        "http://localhost:4000/register",
+        {
+          ...values,
+        },
+        {
+          withCredentials: true,
+        }
+      );
       console.log(data);
       if (data) {
         if (data.errors) {
