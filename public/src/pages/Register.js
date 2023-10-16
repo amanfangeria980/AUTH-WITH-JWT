3a162c109278d42940a308162798bed0643619f2
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 07:12:18 +0530

    designed our register basic page

--- public/src/pages/Register.js
+++ public/src/pages/Register.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="container">
+      <h2>Register Account</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Enter Email" />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Enter Password" />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account? <Link to={"/login"}>Log In</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default Register;
