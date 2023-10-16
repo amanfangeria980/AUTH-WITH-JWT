9dde3c91459c1b1462cd38778c84e020fda4938b
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 22:57:11 +0530

    wrote the business logic for register controller

--- server/Controllers/AuthControllers.js
+++ server/Controllers/AuthControllers.js
@@ -1,2 +1,32 @@
+const UserModel = require("../Models/UserModel");
+const jwt = require("jsonwebtoken");
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, "amanfang", {
+    expiresIn: maxAge,
+  });
+};
+
 module.export.register = async (req, res) => {};
-module.export.login = async (req, res) => {};
+module.export.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.create({
+      email,
+      password,
+    });
+    const token = createToken(user._id);
+    res.cookie("jwt", token, {
+      withCredentials: true,
+      httpOnly: false,
+      maxAge: maxAge * 1000,
+    });
+    res.status(201).json({
+      user: user._id,
+      created: true,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
