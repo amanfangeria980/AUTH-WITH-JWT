020a40c5c9dd10e70365492c82b10c57823c7469
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 07:05:48 +0530

    added our styling in index.css

--- public/src/App.js
+++ public/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
-import Secret from "./pages/Secret";
+import Secret from "./pages/Secret"; 
 import Login from "./pages/Login";
 
 const App = () => {
   return (
     <>
-    <h1>Home Page</h1>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/register" element={<Register />}></Route>
-        <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/secret" element={<Secret />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <h1>Home Page</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/secret" element={<Secret />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
