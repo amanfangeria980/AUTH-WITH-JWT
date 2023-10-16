f02b424c01ffffc731cf7c3afd0b0d2a89d462e6
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-16 07:19:33 +0530

    added states and onchange to our input boxes

--- public/src/pages/Register.js
+++ public/src/pages/Register.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 const Register = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <div className="container">
       <h2>Register Account</h2>
       <form>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" placeholder="Enter Email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={(e) => {
+              setValues({
+                ...values,
+                [e.target.name]: e.target.value,
+              });
+            }}
+          />
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" placeholder="Enter Password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={(e) => {
+              setValues({
+                ...values,
+                [e.target.name]: e.target.value,
+              });
+            }}
+          />
         </div>
         <button type="submit">Submit</button>
         <span>
